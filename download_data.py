ca257a4e45c3bc3da18bd49dcc03edc05c231bce
Ivan Fayos Tormo <ifayost@MacBook-Pro-de-Ivan.local>
2020-04-28 17:12:36 +0200

    Fixed tqdm, vgg16 on gpu and automatic removing duplicate images

--- download_data.py
+++ download_data.py
@@ -1,5 +1,11 @@
 import kaggle
+import shutil
 
 kaggle.api.authenticate()
 
 kaggle.api.dataset_download_files('paultimothymooney/chest-xray-pneumonia', path='./', unzip=True)
+
+try:
+	shutil.rmtree("./chest_xray/chest_xray")
+else:
+	print("Error removing duplicate images.")

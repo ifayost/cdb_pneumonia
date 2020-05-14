7916e1c3aebb1d14039e70399786050435d8c4a9
ifayost <ifayost@gmail.com>
2020-05-14 18:20:31 +0200

    Revert "new data for COVID19"
    This reverts commit 82b67d4b9f4d3668eb6bab6b4a3a2ee7731d0706.

--- download_data.py
+++ download_data.py
@@ -9,12 +9,3 @@ try:
 	shutil.rmtree("./chest_xray/chest_xray")
 except:
 	print("Error removing duplicate images.")
-
-kaggle.api.dataset_download_files('tawsifurrahman/covid19-radiography-database', path='./', unzip=True)
-
-try:
-  shutil.move("./COVID-19 Radiography Database/COVID-19", 
-              "./cdb_pneumonia/chest_xray")
-  shutil.rmtree("./COVID-19 Radiography Database")
-except:
-	print("Error removing non Covid19 images.")
